37b68d98ca7228e16c7314aa11e2eff7415c2cb3
Mkriesberg <mak10097@nyu.edu>
2024-04-10 15:49:27 -0400

    add 4th marker

--- js/markers.js
+++ js/markers.js
@@ -1,6 +1,7 @@
 const train_station = [-73.8298883873377, 40.759575244531234]
 const joes_rice_roll = [-73.82921, 40.75984] 
 const mariscos = [-73.8782069048348, 40.74865358663723]
+const angel = [-73.89097, 40.74721]
 // create the popup
 const popup_trainstation = new mapboxgl.Popup({ offset: 25 }).setText(
     'Flushing-Main Street'
@@ -10,6 +11,9 @@ const popup_breakfast = new mapboxgl.Popup({ offset: 25 }).setText(
 );
 const popup_lunch = new mapboxgl.Popup({ offset: 25 }).setText(
     'Mariscos El Submarino'
+);
+const popup_dinner = new mapboxgl.Popup({ offset: 25 }).setText(
+    'Angel'
 );
  
 // create DOM element for the marker
@@ -21,6 +25,9 @@ const popup_lunch = new mapboxgl.Popup({ offset: 25 }).setText(
 
  const el_mariscos = document.createElement('div');
  el_mariscos.id = 'marker-mariscos';
+
+ const el_angel = document.createElement('div');
+ el_angel.id = 'marker-mariscos';
 
  // create the marker
  new mapboxgl.Marker(el_train_station)
@@ -36,4 +43,9 @@ const popup_lunch = new mapboxgl.Popup({ offset: 25 }).setText(
  new mapboxgl.Marker(el_mariscos)
      .setLngLat(mariscos)
      .setPopup(popup_lunch) // sets a popup on this marker
+     .addTo(map);
+
+ new mapboxgl.Marker(el_angel)
+     .setLngLat(angel)
+     .setPopup(popup_dinner) // sets a popup on this marker
      .addTo(map);
